00cf0b3443c531a68076e466535984f750777647
Wesley McGhee <wesmcghee6372@gmail.com>
2021-10-14 10:07:37 -0500

    initial commit

---
+++ blackjack/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace blackjack
+{
+    class Program
+    {
+        
+       static int deck()
+        {
+            Random random = new Random();
+            int card = random.Next(1, 14);
+
+            if (card >= 10)
+            {
+                return 10;
+            }
+            else
+            {
+                return card;
+            }
+
+        }
+        
+        static void Main(string[] args)
+        {
+            // int dealerSum = 0;
+            // int playerSum = 0;
+            //int[] dealerDeck = { };
+            //int[] playerDeck = { };
+            //  bool gamestatus = true;
+
+            Console.WriteLine(deck());
+        }
+    }
+}
